7026e46d1f3a440fd438879254a079a6d52e46df
cuongtk2002 <cuongtk2002@gmail.com>
2025-03-15 21:40:13 +0700

    feature/chatbot

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,12 +8,12 @@ const Main = () => {
   return (
     <div className='main'>
         <div className="nav">
-            <p>Gemini</p>
+            <p>Legal AI</p>
             <img src={assets.user_icon} alt="" />
         </div>
 
         <div className='main-container'>
-            <div className="greet">
+            {!showResult?<><div className="greet">
                 <p><span>Xin chào</span></p>
                 <p>Hôm nay bạn cần tư vấn gì về pháp luật?</p>
             </div>
@@ -34,14 +34,30 @@ const Main = () => {
                     <p>Tóm tắt một văn bản pháp luật giúp tôi</p>
                     <img src={assets.message_icon} alt="" />
                 </div>
-            </div>
+            </div></>
+            : <div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>:
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>}
+                </div>
+                </div>}
+            
             <div className="main-bottom">
                 <div className="search-box">
                     <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Nhập câu hỏi tại đây" name="" id="" />
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img onClick={()=>onSent()}src={assets.send_icon} alt="" />
+                        {input?<img onClick={()=>onSent()}src={assets.send_icon} alt="" />:null}
                     </div>
                 </div>
                 <div className="bottom-info">
